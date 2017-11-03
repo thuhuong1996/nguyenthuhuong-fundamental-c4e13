3e88aaa714cbf6e56cef4ff5533946dd8876f429
thuhuong1996 <huong.mup.96@gmail.com>
2017-11-03 09:06:35 +0700

    innitial commit

---
+++ c.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a number of row: "))
+b = int(input("Enter a number of column: "))
+for i in range(a):
+        print("x*", end = ' ')
+        print()
---
+++ giaothua.py.py
@@ -0,0 +1,5 @@
+n = int(input("Enter n:"))
+result = 1
+for i in range (1, n+1):
+ result = result  *  i
+print(" The result is: ", result)
